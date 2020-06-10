429098e63c3edc3acd32fc2fc40f8955f68aee67
Sean Stephens <sean.stephens@carvana.com>
2020-06-09 17:51:22 -0700

    updates

--- index.js
+++ index.js
@@ -2,9 +2,14 @@
 'use strict'
 
 module.exports = {
+	root: true,
+
 	extends: [ "react-app" ],
 
 	rules: {
+		"semi": "warn",
+		"no-console":	"warn",
+		"no-debugger": "warn",
 		"react-hooks/exhaustive-deps": "off"
 	}
 };
